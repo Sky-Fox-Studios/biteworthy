1acbb1f08608c154d1df1462cf004a07cd4eaebc
Shadoath <skylar.bolton@gmail.com>
2015-10-02 23:47:43 +0000

    Users got more cool

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,15 +11,15 @@ class Admin::UsersController < ApplicationController
    end
    
    def update
-      respond_to do |format|
-         if @user.update(user_params)
-            format.html { redirect_to root_path, notice: 'User was successfully updated.' }
-            format.json { render :index, status: :ok, location: @user }
-         else
-           format.html { render :edit }
-           format.json { render json: @user.errors, status: :unprocessable_entity }
-         end
+     respond_to do |format|
+       if @user.update(user_params)
+         format.html { redirect_to admin_users_path, notice: 'User was successfully updated.' }
+         format.json { render :index, status: :ok, location: @user }
+       else
+         format.html { render :edit }
+         format.json { render json: @user.errors, status: :unprocessable_entity }
        end
+     end
    end
    
    
@@ -34,6 +34,6 @@ class Admin::UsersController < ApplicationController
    end
    
    def user_params
-      params.require(:user).permit(:name, :email, :password, :is_manager, :is_admin, :approved)
+     params.require(:user).permit(:user_name, :name, :email, :password, :is_manager, :is_admin, :is_editor, :approved)
     end
 end
