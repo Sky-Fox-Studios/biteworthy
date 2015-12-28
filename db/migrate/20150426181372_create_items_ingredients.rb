5144174a8efa7ead3a9fe12ac96f2188a455171c
Skylar Bolton <sbolton@bcimedia.com>
2015-12-28 09:55:53 -0500

    Joins only need two ids

--- db/migrate/20150426181372_create_items_ingredients.rb
+++ db/migrate/20150426181372_create_items_ingredients.rb
@@ -1,7 +1,6 @@
 class CreateItemsIngredients < ActiveRecord::Migration
   def change
     create_table :items_ingredients do |t|
-      t.integer :restaurant_id
       t.integer :item_id
       t.integer :ingredient_id
     end
--- db/migrate/20150426181420_create_foods_ingredients.rb
+++ db/migrate/20150426181420_create_foods_ingredients.rb
@@ -1,7 +1,6 @@
 class CreateFoodsIngredients < ActiveRecord::Migration
   def change
     create_table :foods_ingredients do |t|
-      t.integer  :restaurant_id
       t.integer  :food_id
       t.integer  :ingredient_id
      t.timestamps
