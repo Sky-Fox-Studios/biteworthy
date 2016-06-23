1d00bba785ba510f4448760dfa0ea80c08ca3b3b
Skylar Bolton <sbolton@bcimedia.com>
2016-06-23 17:39:26 -0600

    No public in github
    changed photo to default save location
    
    set symlinks for deploy

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,12 +1,12 @@
-
 class Photo < ActiveRecord::Base
    belongs_to :user
    belongs_to :photo, :polymorphic => true
    has_attached_file :image,
-    styles: { medium: "300x300>", thumb: "100x100>", icon: "32x32>" },
-    default_url: ":rails_root/public/images/:style/missing.png",
-    path: ":rails_root/public/images/:style/:filename"
-   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+    styles: { large: "1000x1000", medium: "300x300>", thumb: "100x100>", icon: "32x32>" },
+    default_url: ":rails_root/public/:style/missing.png",
+    path: ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename"
+
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
    enum image_type: [
      :basic,
@@ -16,7 +16,4 @@ class Photo < ActiveRecord::Base
    def to_param
      "#{id}-#{name.parameterize}"
    end
-
-
-
  end
