f5ee18930600b9c02f0e3a551bedd21b36f04d2a
Skylar Bolton <sbolton@bcimedia.com>
2018-02-06 00:28:01 -0700

    Forgot to add a description

--- db/migrate/20180204002531_add_variety_to_tags.rb
+++ db/migrate/20180204002531_add_variety_to_tags.rb
@@ -1,5 +1,6 @@
 class AddVarietyToTags < ActiveRecord::Migration
   def change
+    add_column :tags, :description, :integer, after: :name
     add_column :tags, :variety, :integer, after: :description
   end
 end
