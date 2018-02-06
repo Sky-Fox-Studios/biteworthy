f4fdfcfd887599e840d5e79edef5540cf91154a1
Skylar Bolton <sbolton@bcimedia.com>
2018-02-06 00:39:43 -0700

    Or did something manually in the old DB migrations

--- db/migrate/20180204002531_add_variety_to_tags.rb
+++ db/migrate/20180204002531_add_variety_to_tags.rb
@@ -1,6 +1,5 @@
 class AddVarietyToTags < ActiveRecord::Migration
   def change
-    add_column :tags, :description, :integer, after: :name
     add_column :tags, :variety, :integer, after: :description
   end
 end
