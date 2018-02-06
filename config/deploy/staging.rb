924920be31bffea40b1e71758196b442ea1cb773
Skylar Bolton <sbolton@bcimedia.com>
2018-02-06 00:24:08 -0700

    Production/staging settings

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,8 @@
-role :app, %w{deploy@45.55.118.103}
-role :db,  %w{deploy@45.55.118.103}
-role :web, %w{deploy@45.55.118.103}
+role :app, %w{deploy@52.27.124.36}
+role :db,  %w{deploy@52.27.124.36}
+role :web, %w{deploy@52.27.124.36}
 
-server '45.55.118.103', user: 'deploy', roles: %w{web app db}
+server '52.27.124.36', user: 'ubuntu', roles: %w{web app db}
 set :branch, 'staging'
 set :deploy_to, "/data/#{fetch(:application)}/staging"
 set :rails_env, 'staging'
