3fa6db2d19b38426bfd5ab20e4ac7340b52a6236
Skylar Bolton <sbolton@bcimedia.com>
2018-02-17 00:37:13 -0700

    updated paperclip options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,21 +1,7 @@
 Rails.application.configure do
   config.app_version = ".6.0"
 
-   Paperclip.options[:command_path] = "/usr/bin/convert/"
-   Paperclip::Attachment.default_options.merge!({
-     path: "images/production/:class/:id/:attachment/:style/img_:fingerprint",
-     storage: :fog,
-     fog_credentials: {
-       provider:           'Rackspace',
-       rackspace_username: ENV['rackspace_username'],
-       rackspace_api_key:  ENV['rackspace_api_key'],
-       rackspace_region:   :dfw,
-       persistent:         true
-     },
-     fog_directory: ENV['rackspace_cdn_dir'],
-     fog_public:    true,
-     fog_host:      ENV['rackspace_cdn_name']
-   })
+  Paperclip.options[:command_path] = "/usr/bin/convert/"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,7 +2,7 @@
 Paperclip::Attachment.default_options.update(
   storage: :s3,
   url: ':s3_domain_url',
-  path: '/#{Rails.env}/photos/:attachment/:id_partition/:style/:filename',
+  path: "/#{Rails.env}/photos/:attachment/:id_partition/:style/:filename",
   s3_credentials: {
     bucket: 'biteworthy',
     access_key_id: ENV['aws_access_key_id'],
