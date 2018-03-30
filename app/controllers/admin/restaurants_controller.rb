af7fe74af5e929fe3680dca02a96c8e1d4ed27d8
Skylar Bolton <sbolton@bcimedia.com>
2018-03-29 22:44:09 -0600

    Allow Restauarant[:website] update in controller

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -58,7 +58,7 @@ class Admin::RestaurantsController < AdminController
     end
 
     def restaurant_params
-      params.require(:restaurant).permit(:name, :slogan, :about, :disclaimer, :inside_seating, :outside_seating, :cash_only, :delivers, :active,
+      params.require(:restaurant).permit(:name, :slogan, :about, :disclaimer, :inside_seating, :outside_seating, :cash_only, :delivers, :active, :website,
         addresses_attributes: [:restaurant_id, :phone_number, :street, :city, :state, :zip, :latitude, :longitude])
     end
 end
