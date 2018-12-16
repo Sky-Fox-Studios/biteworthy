cb6f4898cf0ad419b3bb245078dccbe2d5dbe646
Skylar Bolton <sbolton@bcimedia.com>
2018-12-16 16:49:34 -0700

    Clean up

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,23 +1,13 @@
 class ReportsController < ApplicationController
   before_action :set_report, only: [:show, :edit, :update, :destroy]
 
-  # GET /reports
-  # GET /reports.json
-  def index
-    @reports = Report.all
-  end
-
-  # GET /reports/new
   def new
     @report = Report.new
   end
 
-  # GET /reports/1/edit
   def edit
   end
 
-  # POST /reports
-  # POST /reports.json
   def create
     @report = Report.new(report_params)
 
@@ -27,30 +17,6 @@ class ReportsController < ApplicationController
       else
         format.html { render :new }
       end
-    end
-  end
-
-  # PATCH/PUT /reports/1
-  # PATCH/PUT /reports/1.json
-  def update
-    respond_to do |format|
-      if @report.update(report_params)
-        format.html { redirect_to @report, notice: 'Report was successfully updated.' }
-        format.json { render :show, status: :ok, location: @report }
-      else
-        format.html { render :edit }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /reports/1
-  # DELETE /reports/1.json
-  def destroy
-    @report.destroy
-    respond_to do |format|
-      format.html { redirect_to reports_url, notice: 'Report was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
