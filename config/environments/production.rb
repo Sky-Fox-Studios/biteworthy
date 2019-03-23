9e5cce970d1f1561387dd8678f43b7d125d8c75a
Skylar Bolton <sbolton@bcimedia.com>
2019-03-23 13:36:32 -0600

    Send mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,9 +63,8 @@ Rails.application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.smtp_settings = { address: "smtp-relay.gmail.com", port: 587 }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
