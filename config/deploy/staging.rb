9a8d33c994d41bdd40a8f965033ec11d68ab7015
Skylar Bolton <sbolton@bcimedia.com>
2019-06-27 21:24:39 -0600

    Update where BWD deploys

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 server '52.27.124.36', user: 'ubuntu', roles: %w{web app db}
 set :branch, 'sb-points'
-set :deploy_to, "/data/#{fetch(:application)}/staging"
+set :deploy_to, "/data/#{fetch(:application)}_staging"
 set :rails_env, 'staging'
