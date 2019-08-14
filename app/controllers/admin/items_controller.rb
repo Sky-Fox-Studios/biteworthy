e8b1d1d205957dd344f92dd56653a9dd85bc6faa
Skylar Bolton <sbolton@bcimedia.com>
2019-08-14 09:20:56 -0600

    Extra Polymoriphic changes

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -90,13 +90,22 @@ class Admin::ItemsController < AdminController
   end
 
   def add_new_extra
-    if params[:extra_name].present?
-      extra = Extra.find_or_create_by(name: params[:extra_name], description: params[:extra_description], addon_type: params[:addon_type], restaurant: @item.restaurant)
+    # From admin_extras_controller
+    keys = params[:extra].keys.select{|x| x.match(/extrable_*/) && x != "extrable_id"}
+    keys.each do |key|
+      if params[:extra][key].present?
+        params[:extra][:extrable_id] = params[:extra][key].split(":").last
+        params[:extra][:extrable_type] = params[:extra][key].split(":").first
+      end
+    end
+
+    if params[:extra][:extrable_id].present? && params[:extra][:extrable_type].present?
+      extra = Extra.find_or_create_by(extrable_id: params[:extra][:extrable_id], extrable_type: params[:extra][:extrable_type], addon_type: Extra.addon_types[params[:addon_type]], restaurant: @item.restaurant)
       if extra && extra.valid?
-        if @item.extras.include? extra
-          @notice = "#{@item.name} already has #{params[:addon_type]} of  #{params[:extra_name]}"
-        else
+        if @item.extras.select{ |e| e.extrable_id == extra.extrable_id && e.addon_type == extra.addon_type}.empty?
           @item.extras << extra
+        else
+          @notice = "#{@item.name} already has #{params[:addon_type]} of #{extra.extrable.name}"
         end
       elsif extra
         @notice = extra.errors.full_messages.to_sentence
@@ -120,10 +129,10 @@ class Admin::ItemsController < AdminController
     end
 
     if extra.present?
-      if !@item.extras.include?(extra)
+      if @item.extras.select{ |e| e.extrable_id == extra.extrable_id && e.addon_type == extra.addon_type}.empty?
         @item.extras << extra
       else
-        @notice = "#{@item.name} already has #{extra.addon_type} of #{extra.name}"
+        @notice = "#{@item.name} already has #{extra.addon_type} of #{extra.extrable.name}"
       end
     end
 
