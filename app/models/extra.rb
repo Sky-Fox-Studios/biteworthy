7236994fa6e1accbef4e19fe20354b3b2e53ceff
Skylar Bolton <sbolton@bcimedia.com>
2019-08-28 07:45:10 -0600

    Unique scope for Extra

--- app/models/extra.rb
+++ app/models/extra.rb
@@ -16,7 +16,7 @@ class Extra < ActiveRecord::Base
   validates :restaurant_id, :extrable_id, :extrable_type, :addon_type, presence: true
 
   # TODO think more here
-  # validates_uniqueness_of :name, scope: [:restaurant_id, :addon_type]
+  validates_uniqueness_of :extrable_id, :extrable_type, :addon_type, scope: [:restaurant_id, :extrable_id, :extrable_type, :addon_type]
 
   def to_param
     "#{id}-#{extrable.name.parameterize}"
